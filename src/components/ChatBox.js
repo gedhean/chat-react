76f68954da9fb16fb062e6ca23bd945070a291b8
Gêdhean Alves Vieira <gedhean1@gmail.com>
2018-04-06 22:47:26 -0300

    trabalhando no conteudo do chat

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ChaHeader from './ChaHeader';
 import ChatLogin from './ChatLogin';
+import ChatContent from './ChatContent';
 import './chatBox.css';
 
 const ChatBox = (props) => {
     return (
         <div className="chat-box">
             <ChaHeader />
+            <ChatContent />
             <ChatLogin />
         </div>
     )
---
+++ src/components/ChatContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './chatContent.css';
+
+export default class ChatContent extends React.Component {
+    render() {
+        return (
+            <div className="chat-content">
+               
+            </div>
+        );
+    }
+}
