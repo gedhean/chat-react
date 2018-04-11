62007e467b299c2e5a05735c1ffbba2a87c4ad24
Gêdhean Alves Vieira <gedhean1@gmail.com>
2018-04-11 19:41:37 -0300

    ChatMessage component refecored

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './chatMessage.css';
 
-const ChatMessage = (props) => {
-    // msg is the message content
-    const msg = props.msg;
-    // time is the time when the msg was sent
-    const time = props.time;
-    // from is who sent the msg (agente or client)
-    const from = props.from;
+const ChatMessage = ({
+    msg,    // msg is the message content
+    time,   // time is the time when the msg was sent
+    from    // from is who sent the msg (agente or client)
+}) => {
     // to set diferent styles for client and agente
     const baloonClass = `msg-container ${from}`;
 
