68701777940b7e9bc79bd8a91e3c9e28d630df96
Gêdhean Alves Vieira <gedhean1@gmail.com>
2018-04-19 21:58:45 -0300

    code formated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { connect } from "react-redux";
-import io from "socket.io-client";
+import React from 'react';
+import { connect } from 'react-redux';
+import io from 'socket.io-client';
 
-import { login, setSocket } from "../redux/actions";
-import "./login.css";
+import { login, setSocket } from '../redux/actions';
+import './login.css';
 
 /**
  * The socket MUST be instantiated outside the componentes!
  */
-const SOCKET = io("http://localhost:8000");
+const SOCKET = io('http://localhost:8000');
 const localStorage = window.localStorage;
 
 class Login extends React.Component {
   state = {
-    name: "",
-    email: ""
+    name: '',
+    email: ''
   };
 
   componentDidMount() {
-    const logged = localStorage.getItem("isLogged");
-    if (logged === "true") {
-      log("O usuario já logou");
+    const logged = localStorage.getItem('isLogged');
+    if (logged === 'true') {
+      log('O usuario já logou');
       const { dispatch } = this.props;
       dispatch(setSocket(SOCKET));
       dispatch(login());
@@ -37,19 +37,19 @@ class Login extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    localStorage.setItem("isLogged", "true");
+    localStorage.setItem('isLogged', 'true');
 
     const { dispatch } = this.props;
 
     dispatch(setSocket(SOCKET));
     dispatch(login());
 
-    log("Login submetido");
+    log('Login submetido');
   };
 
   componentWillUnmount() {
-    window.removeEventListener("submit", this.handleSubmit, false);
-    window.removeEventListener("change", this.handleChange, false);
+    window.removeEventListener('submit', this.handleSubmit, false);
+    window.removeEventListener('change', this.handleChange, false);
   }
 
   render() {
@@ -92,5 +92,5 @@ class Login extends React.Component {
 export default connect(null)(Login);
 //Debbug
 const log = str => {
-  console.log("At Login:", str);
+  console.log('At Login:', str);
 };
